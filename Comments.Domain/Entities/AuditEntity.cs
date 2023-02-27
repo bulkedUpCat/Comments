4533244929f32cc13798a44b2e0bb2e6eca3c5c3
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-02-27 14:20:55 +0200

    Adds basic entities

---
+++ Comments.Domain/Entities/AuditEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Comments.Domain.Entities
+{
+    public class AuditEntity<TId>: EntityBase<TId>
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
