9ec74ff53fb1f6cd618f6a49df71a24ab967e6dc
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-02-27 21:00:22 +0200

    Add CommentsContext

---
+++ Comments.Infrastructure/Data/CommentsContext.cs
@@ -0,0 +1,12 @@
+﻿using Comments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Comments.Infrastructure.Data
+{
+    public class CommentsContext: DbContext
+    {
+        public CommentsContext(DbContextOptions<CommentsContext> options): base(options){}
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
